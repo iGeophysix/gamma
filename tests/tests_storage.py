4edbdcb716e17ec8cc71da253ac9e500acb45ce2
Alexander Filimonov <me@igeophysix.com>
2021-03-21 17:18:02 +0300

    + tests for append_log_meta

--- tests/tests_storage.py
+++ tests/tests_storage.py
@@ -21,3 +21,25 @@ class TestStorage(unittest.TestCase):
         self.assertEqual(self._s.get_well_info("test_well"), {})
         self._s.delete_well("test_well")
         self.assertNotIn("test_well", self._s.list_wells())
+
+    def test_append_log_meta(self):
+        wellname = "test_well"
+        datasetname = "test_dataset"
+        logname = "GR"
+        self._s.create_well(wellname)
+        self._s.create_dataset(wellname, datasetname)
+        self._s.add_log(wellname, datasetname, logname)
+        current_meta = self._s.get_logs_meta(wellname, datasetname, [logname, ])
+        self.assertEqual({'GR': {}}, current_meta)
+
+        extra_meta = {"UWI": 434232}
+        self._s.append_log_meta(wellname, datasetname, logname, extra_meta)
+        self.assertEqual({'GR': {"UWI": 434232}}, self._s.get_logs_meta(wellname, datasetname, [logname, ]))
+
+        extra_meta = {"PWA": "GOGI"}
+        self._s.append_log_meta(wellname, datasetname, logname, extra_meta)
+        self.assertEqual({'GR': {"UWI": 434232, "PWA": "GOGI"}}, self._s.get_logs_meta(wellname, datasetname, [logname, ]))
+
+        extra_meta = {"PWA": "GIGI"}
+        self._s.append_log_meta(wellname, datasetname, logname, extra_meta)
+        self.assertEqual({'GR': {"UWI": 434232, "PWA": "GIGI"}}, self._s.get_logs_meta(wellname, datasetname, [logname, ]))
