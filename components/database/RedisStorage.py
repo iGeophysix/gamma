normalization fix

components/database/RedisStorage.py
@@ -371,13 +371,9 @@ class RedisStorage:
             # convert np.array to bytes
             mapping = {}
             for k, v in data.items():
-                try:
-                    non_null_values = v[~np.isnan(v[:, 1])]
-                except TypeError:  # string arrays cannot be compared with nan - so register everything
-                    non_null_values = v
                 stream = io.BytesIO()
                 # np.savez_compressed(stream, array=v)
-                np.save(stream, non_null_values, allow_pickle=True)
+                np.save(stream, v, allow_pickle=True)
                 mapping[k] = stream.getvalue()  # bytes
 
             self.conn.hset(dataset_id, mapping=mapping)

components/petrophysics/normalization.py
@@ -37,7 +37,7 @@ def log_normalization(w_wd_log: list[tuple[str, str, str]]) -> [dict, dict]:
     # define ranking and median quantiles
     rank = sorted(deviations, key=deviations.get)
 
-    def get_quantiles(_logs: dict[BasicLog], _ranked_logs: list, quantiles: tuple = (5, 95)) -> dict[float, float]:
+    def get_quantiles(_logs: dict[tuple, BasicLog], _ranked_logs: list, quantiles: tuple = (5, 95)) -> dict[float, float]:
         """
         This internal function averages histograms and returns quantile values
         """
@@ -59,6 +59,7 @@ def log_normalization(w_wd_log: list[tuple[str, str, str]]) -> [dict, dict]:
     etalon_q_min, etalon_q_max = etalon_quantiles.values()
 
     etalon_data = logs[rank[0]][:, 1]
+
     etalon_histogram = np.histogram(etalon_data, bins=20, range=(etalon_data.min(), etalon_data.max()), density=True)
     results = {}
     for key, log in logs.items():
@@ -67,7 +68,7 @@ def log_normalization(w_wd_log: list[tuple[str, str, str]]) -> [dict, dict]:
         k = (etalon_q_max - etalon_q_min) / (q_max - q_min)
         new_values = (log[:, 1] - q_min) * k + etalon_q_min
 
-        new_histogram = np.histogram(log.values[:, 1], bins=20, range=(etalon_data.min(), etalon_data.max()), density=True)
+        new_histogram = np.histogram(new_values, bins=20, range=(etalon_data.min(), etalon_data.max()), density=True)
         distribution_similarity = sum(abs(new_histogram[0] - etalon_histogram[0]))
         extra_meta = {"normalization": {'difference': distribution_similarity}}
 

components/petrophysics/tests/test_log_normalization.py
@@ -49,7 +49,7 @@ class TestLogNormalization(unittest.TestCase):
             log.save()
             log.history = f"Normalized from {w_wd_log}"
             log.save()
-            q5 = np.quantile(log[:, 1], 0.05)
-            q95 = np.quantile(log[:, 1], 0.95)
+            q5 = np.quantile(log.values[~np.isnan(log.values[:, 1])][:, 1], 0.05)
+            q95 = np.quantile(log.values[~np.isnan(log.values[:, 1])][:, 1], 0.95)
             self.assertAlmostEqual(true_q5, q5, places=4)
             self.assertAlmostEqual(true_q95, q95, places=4)