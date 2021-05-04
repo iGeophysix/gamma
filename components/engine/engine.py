76450625b8978856f1071a054ec913739f82a3f2
Alexander Filimonov <me@igeophysix.com>
2021-05-04 17:10:30 +0300

    + ProjectStatisticsNode

--- components/engine/engine.py
+++ components/engine/engine.py
@@ -2,6 +2,7 @@ from components.importexport.FamilyAssigner import FamilyAssignerNode
 from components.petrophysics.best_log_detection import BestLogDetectionNode
 from components.petrophysics.curve_operations import BasicStatisticsNode, LogResolutionNode
 from components.petrophysics.normalization import LogNormalizationNode
+from components.petrophysics.project_statistics import ProjectStatisticsNode
 from components.petrophysics.run_detection import RunDetectionNode
 from components.petrophysics.volumetric_model import VolumetricModelSolverNode
 
@@ -12,7 +13,8 @@ NODES = {
     'FamilyAssignerNode': FamilyAssignerNode,
     'BestLogDetectionNode': BestLogDetectionNode,
     'LogNormalizationNode': LogNormalizationNode,
-    'VolumetricModelSolverNode': VolumetricModelSolverNode
+    'VolumetricModelSolverNode': VolumetricModelSolverNode,
+    'ProjectStatisticsNode': ProjectStatisticsNode,
 }
 
 
@@ -20,16 +22,17 @@ class Engine:
     """
     Class that launches EngineNodes
     """
+
     steps = [
         {'node': 'BasicStatisticsNode', 'parameters': {}},
         {'node': 'LogResolutionNode', 'parameters': {}},
         {'node': 'RunDetectionNode', 'parameters': {}},
         {'node': 'FamilyAssignerNode', 'parameters': {}},
         {'node': 'BestLogDetectionNode', 'parameters': {}},
+        {'node': 'ProjectStatisticsNode', 'parameters': {}},
         {'node': 'LogNormalizationNode', 'parameters': {'lower_quantile': 0.05, 'upper_quantile': 0.95}},
         {'node': 'VolumetricModelSolverNode', 'parameters': {'log_families': ['Gamma Ray', 'Bulk Density', 'Thermal Neutron Porosity'], 'model_components': ['Shale', 'Quartz', 'Calcite', 'Water']}},
     ]
-
     def start(self):
         for step in self.steps:
             print(f'Starting {step}')
