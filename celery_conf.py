a324c83ad912e52e6b01a8f7a3c05e2f406b3202
Alexander Filimonov <me@igeophysix.com>
2021-05-04 19:04:14 +0300

    refactored imports

--- celery_conf.py
+++ celery_conf.py
@@ -2,8 +2,7 @@ import os
 
 from celery import Celery
 from celery.result import AsyncResult
-
-from components.database.settings import REDIS_HOST
+from settings import REDIS_HOST
 
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
 REDIS_DB = os.environ.get('REDIS_PORT', 0)
@@ -12,6 +11,7 @@ app.conf.result_backend = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}'
 app.conf.result_expires = 60
 app.conf.task_serializer = 'pickle'
 app.conf.accept_content = ['pickle', 'json']
+
 
 def get_running_tasks():
     i = app.control.inspect()
