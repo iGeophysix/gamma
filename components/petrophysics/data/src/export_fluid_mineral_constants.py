b04e994c92233422146d95b02f55be3f80a0d6df
Anton Ovchinnikov <ollypc@gmail.com>
2021-07-25 14:27:33 +0500

    Update export routine of FLUID_MINERAL_TABLE

--- components/petrophysics/data/src/export_fluid_mineral_constants.py
+++ components/petrophysics/data/src/export_fluid_mineral_constants.py
@@ -6,19 +6,20 @@ import os
 from components.database.RedisStorage import RedisStorage
 
 SOURCE_FLUID_MINERAL_TABLE = os.path.join(os.path.dirname(__file__), 'FluidMineralConstants.xlsx')
-EXPORT_FLUID_MINERAL_TABLE = os.path.join(os.path.dirname(__file__), 'FluidMineralConstants.json')
 FLUID_MINERAL_TABLE = 'fluid_mineral_constants'
+
 
 def build_fluid_mineral_constants():
     # read Excel table
     with open(SOURCE_FLUID_MINERAL_TABLE, 'rb') as f:
         df = pandas.read_excel(f, 'Constants', header=0, index_col=0)
+    df.fillna(0, inplace=True)
 
     # export table as JSON
-
     s = RedisStorage()
     s.object_delete(FLUID_MINERAL_TABLE)
     s.object_set(FLUID_MINERAL_TABLE, json.dumps(df.to_dict(orient='index')))
+
 
 if __name__ == '__main__':
     build_fluid_mineral_constants()
